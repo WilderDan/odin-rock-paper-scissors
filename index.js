4892711965dad50018231d49c38a4af6099bf7ba
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-08-19 18:13:44 -0500

    Add function getComputerChoice

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const getComputerChoice = () => {
+  let options = ["Rock", "Paper", "Scissors"];
+  let choice = getRandomInt(0, 2);
+
+  return options[choice];
+};
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
