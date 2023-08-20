3355c1577618b15eef3fba29452bd15619307ca3
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-08-20 00:02:16 -0500

    Add function playRound and helpers

--- index.js
+++ index.js
@@ -8,3 +8,48 @@ const getComputerChoice = () => {
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  let player = playerSelection.toUpperCase();
+  let computer = computerSelection.toUpperCase();
+
+  if (player === computer) {
+    return 0;
+  }
+
+  if (player === "ROCK") {
+    return computer === "PAPER" ? -1 : 1;
+  } else if (player === "PAPER") {
+    return computer === "ROCK" ? 1 : -1;
+  } else if (player === "SCISSORS") {
+    return computer === "ROCK" ? -1 : 1;
+  }
+};
+
+const getResultMessage = (playerSelection, result) => {
+  let selection = playerSelection.toUpperCase();
+
+  if (result === 0) {
+    return "Tie!";
+  }
+
+  if (result === 1) {
+    return "You win! " + selection + " beats " + getWinsAgainst(selection);
+  }
+
+  if (result === -1) {
+    return "You Lose! " + getLosesAgainst(selection) + " beats " + selection;
+  }
+};
+
+const getWinsAgainst = (selection) => {
+  let s = selection.toUpperCase();
+
+  return s === "ROCK" ? "SCISSORS" : s === "PAPER" ? "ROCK" : "PAPER";
+};
+
+const getLosesAgainst = (selection) => {
+  let s = selection.toUpperCase();
+
+  return s === "ROCK" ? "PAPER" : s === "PAPER" ? "SCISSORS" : "ROCK";
+};
